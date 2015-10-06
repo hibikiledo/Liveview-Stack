25c9e7fb4ddf3f895e8dc629769442bd0579020a
@hbk <nut.rainbowpuke@gmail.com>
2015-10-06 09:32:44 +0700

    Partially implement graceful shutdown.

--- liveviewstack.py
+++ liveviewstack.py
@@ -1,13 +1,20 @@
 import eventlet
 
 import os
+import signal
 
 from multiprocessing import Process, RLock
 from PIL import Image
 
 
+running = True
+
+
 def eater(ip, port, output_partial_path, output_path):
-    while True:
+
+    print("eater started")
+
+    while running:
         s = eventlet.connect((ip, port))
         output_bytes = bytearray()
 
@@ -23,17 +30,24 @@ def eater(ip, port, output_partial_path, output_path):
 
         os.rename(output_partial_path, output_path)
 
+    print("eater stopped")
+
 
 def digester(input_path, output_partial_path, output_path):
-    while True:
+
+    print("digester started")
+
+    while running:
         im = Image.open(input_path)
         im.thumbnail((640, 480), Image.ANTIALIAS)
         im.save(output_partial_path)
 
         os.rename(output_partial_path, output_path)
 
+    print("digester stopped")
 
-def streamer_handler(client_socket, address, image_path, streamer_lock):
+
+def streamer_handler(client_socket, address, image_path):
     print("Handling", address)
     image_file = open(image_path, 'rb')
     image_bytes = image_file.read()
@@ -41,16 +55,37 @@ def streamer_handler(client_socket, address, image_path, streamer_lock):
     client_socket.close()
 
 
-def streamer(streamer_lock, port, src_path):
+def streamer(port, src_path):
     server_socket = eventlet.listen(('0.0.0.0', port))
     pool = eventlet.GreenPool(10)
-    while True:
+    while running:
         client_socket, address = server_socket.accept()
-        pool.spawn_n(streamer_handler, client_socket, address, src_path, streamer_lock)
+        pool.spawn_n(streamer_handler, client_socket, address, src_path)
+
+
+def SIGINT_handler(signum, frame):
+    global running
+    running = False
 
 
 if __name__ == '__main__':
+
+    # Setup signal handler
+    signal.signal(signal.SIGINT, handler=SIGINT_handler)
+
     # Start all processes
-    Process(target=eater, args=('192.168.2.100', 1234, 'tmp/full.jpg.part', 'tmp/full.jpg')).start()
-    Process(target=digester, args=('tmp/full.jpg', 'tmp/stream.jpg.part', 'tmp/stream.jpg')).start()
-    Process(target=streamer, args=(1234, 'tmp/stream.jpg')).start()
+    a = Process(target=eater, args=('192.168.2.100', 1234, 'tmp/full.partial.jpg', 'tmp/full.jpg'))
+    b = Process(target=digester, args=('tmp/full.jpg', 'tmp/stream.partial.jpg', 'tmp/stream.jpg'))
+    c = Process(target=streamer, args=(1234, 'tmp/stream.jpg'))
+
+    # Start all process
+    a.start()
+    b.start()
+    c.start()
+
+    # Join all process back for clean termination
+    a.join()
+    b.join()
+    c.join()
+
+
