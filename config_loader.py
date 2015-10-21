d6d82d93bc448859ddc80b481242ff9f681b1132
@hbk <nut.rainbowpuke@gmail.com>
2015-10-21 18:16:12 +0700

    Add comments

--- config_loader.py
+++ config_loader.py
@@ -3,6 +3,9 @@
 
     This module provide a convenient method for reading configuration file.
     It returns dictionary as a result of the reading.
+
+    Format of configuration file
+      key=value
 '''
 
 
@@ -17,11 +20,12 @@ def load(filename):
         # if length of line is 0, then EOF has reached
         if len(line) == 0:
             break
+        # if length of line is 0, then it is probably just newline char
         if len(line) == 1:
             continue
         # split at = sign
         tokens = line.split("=")
-        tokens_cleaned = [ token.replace("\n", "") for token in tokens ]
+        tokens_cleaned = [token.replace("\n", "") for token in tokens]
         # update value into config dict
         config_dict[tokens_cleaned[0]] = tokens_cleaned[1]
 
